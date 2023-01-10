5d637ead5c4fb741a0778ceb4695b8bbe46547c7
leonardopadilha <leonardopadilha28@gmail.com>
2023-01-10 07:09:12 -0300

    Iniciando separação dos serviços

--- api/services/Services.js
+++ api/services/Services.js
@@ -1,13 +1,29 @@
-const database = require('../models')
+const database = require("../models");
 
 class Services {
-    constructor(nomeDoModelo) {
-        this.nomeDoModelo = nomeDoModelo
-    }
+  constructor(nomeDoModelo) {
+    this.nomeDoModelo = nomeDoModelo;
+  }
 
-    async pegaTodosOsRegistros() {
-        return database[this.nomeDoModelo].findAll()
-    }
+  async pegaTodosOsRegistros() {
+    return database[this.nomeDoModelo].findAll()
+  }
+
+  async pegaUmRegistro(id) {
+
+  }
+
+  async criaRegistro(dados) {
+
+  }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+
+  }
+
+  async apagaRegistro(id) {
+    
+  }
 }
 
-module.exports = Services
+module.exports = Services;
